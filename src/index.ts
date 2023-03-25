72b4f14a9f721b1d717d5af6c32d056f3eac4fb8
Nafis Handoko <naffisioner@gmail.com>
2023-03-25 13:25:03 +0700

    remove console.log

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ app.get('/test', (req: Request, res: Response) => {
 app.post('/', async (req: Request, res: Response) => {
     try {
         const prompt = req.body.prompt;
-        console.log(prompt)
+        // console.log(prompt)
 
         const response = await openai.createCompletion({
             model: "text-davinci-003",
@@ -33,7 +33,7 @@ app.post('/', async (req: Request, res: Response) => {
             frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
             presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
         });
-        console.log(response.data.choices[0].text)
+        // console.log(response.data.choices[0].text)
 
         res.status(200).send({
             bot: response.data.choices[0].text
