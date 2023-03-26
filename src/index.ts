be864ad0ad497bde98162650a3599c505a491321
Nafis Handoko <naffisioner@gmail.com>
2023-03-26 19:21:23 +0700

    make it context aware by using gpt3 chat model

--- src/index.ts
+++ src/index.ts
@@ -21,14 +21,14 @@ app.get('/test', (req: Request, res: Response) => {
 
 app.post('/', async (req: Request, res: Response) => {
     try {
-        const prompt = req.body.prompt;
+        const messages = req.body.messages;
         // console.log(prompt)
 
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `${prompt}`,
-            temperature: 0, // Higher values means the model will take more risks.
-            max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+        const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: messages,
+            temperature: 1, // Higher values means the model will take more risks.
+            max_tokens: 1500, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
             top_p: 1, // alternative to sampling with temperature, called nucleus sampling
             frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
             presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
@@ -36,7 +36,7 @@ app.post('/', async (req: Request, res: Response) => {
         // console.log(response.data.choices[0].text)
 
         res.status(200).send({
-            bot: response.data.choices[0].text
+            message: response.data.choices[0].message?.content
         });
 
     } catch (error) {
